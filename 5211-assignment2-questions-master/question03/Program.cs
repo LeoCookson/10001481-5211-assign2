9057b98c823e84e0159b2cf29e55ed82ff103ed9
LeoCookson <leocookson@outlook.com>
2018-05-30 11:58:56 +1200

    Q3_Added_stopwatch_Lines_11_13_19_22_24_32_33_added_System_diagnostics_line_3_changed_string_name_line26

---
+++ 5211-assignment2-questions-master/question03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace question03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+              
+                 sw.Start();
+         
+            string[] fruitsArray = new string[] {"Banana", "Grapefruit", "Tomato", "Oranges"};
+
+          
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+
+            sw.Reset();
+
+            sw.Start();
+                
+            List<string> fruitsArray2 = new List<string> {"Banana", "Grapefruit", "Tomato", "Oranges"};
+
+              foreach(var fruit in fruitsArray) {
+                Console.WriteLine(fruit);
+            }
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+        }
+    }
+}
