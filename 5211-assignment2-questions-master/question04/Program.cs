b09001bcf866ffbbf6167f4b2c225bad7afa3287
LeoCookson <leocookson@outlook.com>
2018-06-05 08:07:14 +1200

    Q4_Added_TryParse_Line_11

---
+++ 5211-assignment2-questions-master/question04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace question04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print out the reverse timestables (highest number first) for:");
+
+            var number = int.TryParse(Console.ReadLine(), out int Number);
+
+            for(var i = 0; i < 12; i++) {
+                int a = i + 1;
+                Console.WriteLine($"{a} x {Number} = {a * Number}");
+            }
+        }
+    }
+}
